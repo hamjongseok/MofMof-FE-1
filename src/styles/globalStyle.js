ccbda3d969dc2b9b4f2f095dbc282a5c9df90b9c
hyunwlee-dev <hwlee3928@gmail.com>
2023-05-07 19:34:17 +0900

    style: common variable 추가

--- src/styles/globalStyle.js
+++ src/styles/globalStyle.js
@@ -1,6 +1,9 @@
 import {createGlobalStyle} from 'styled-components';
+import {variables} from './utils';
 
 export const GlobalStyle = createGlobalStyle`
+  ${variables}
+
 	body{
 		margin: 0;
 		padding: 0;
