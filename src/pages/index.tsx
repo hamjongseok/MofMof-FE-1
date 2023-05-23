2c108e9c0153ff78d907d5b61eae744aa5905a32
hamjongseok <irang4605@naver.com>
2023-05-23 14:00:17 +0900

    refactor: 사용하지않는것 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,6 @@
 import Link from 'next/link';
 import styled from 'styled-components';
-import Button from '@/components/UI/Button';
-import Header from '@/components/Header';
-import Footer from '@/components/Footer';
 import BaseLayout from '@/components/Layout';
-
-const StyledDiv = styled.div`
-  background-color: #fff;
-  max-width: 1200px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
 
 const StyledLink = styled.a`
   color: #ff5500;
--- src/styles/Header/MobileDropdownStyle.ts
+++ src/styles/Header/MobileDropdownStyle.ts
@@ -1,4 +1,4 @@
-import styled, {keyframes, css} from 'styled-components';
+import styled, {keyframes} from 'styled-components';
 
 // interface mobileDropdown {
 //   mobileDropdown: string | null;
